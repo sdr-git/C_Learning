8d42d6ccd8c4fc0956078fd87ec08eee623f058d
SDR-git <2191398297@qq.com>
2026-06-28 15:35:16 +0800

    2026/6/28oj1005完成

---
+++ oj_1005/oj_1005/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void)
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%-9d%-9d%-9d\n", a, a*a, a*a*a);
+	printf("%-9d%-9d%-9d\n", b, b*b, b*b*b);
+	printf("%-9d%-9d%-9d\n", c, c*c, c*c*c);
+	return 0;
+}
