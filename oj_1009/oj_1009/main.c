c633e2fb74c2936f8e4ff55b1ae4a493cc2bccea
SDR-git <2191398297@qq.com>
2026-07-08 11:27:06 +0800

    2026/7/8oj1009完成

---
+++ oj_1009/oj_1009/main.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void)
+{
+	int a, b, c;
+	float p;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%.2f", p = (a + b + c)/3.0);
+	return 0;
+}
