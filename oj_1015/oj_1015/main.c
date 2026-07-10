f6d6b661c03fedb43367dd77285d4070c14f3638
SDR-git <2191398297@qq.com>
2026-07-10 13:59:31 +0800

    2026/7/10oj1015

---
+++ oj_1015/oj_1015/main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	double a, b, c;
+	double t, x1, x2;
+	scanf("%lf %lf %lf", &a, &b, &c);
+	t = sqrt(b * b - (4 * a * c));
+	x1 = (-b + t) / (2 * a);
+	x2 = (-b - t) / (2 * a);
+	if (x1 > x2)
+		printf("%7.2lf%7.2lf", x1, x2);
+	else
+		printf("%7.2lf%7.2lf", x2, x1);
+	return 0;
+}
